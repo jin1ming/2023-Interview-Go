6db0fd302c04ad4e4343bfeeb543b7c3a61b6bd8
jin1ming <jin1ming@vip.qq.com>
2021-07-12 11:11:58 +0800

    7.12

--- Algorithms.go
+++ Algorithms.go
@@ -431,11 +431,11 @@ func reorderList(head *ListNode)  {
 
 	head2 := p.Next
 	p.Next = nil
-
+	// 将后半段逆转
 	head2 = reverse(head2)
 
 	p = head
-
+	// 依次插入
 	var tmp *ListNode
 	for p != nil && head2 != nil {
 		tmp = p.Next
@@ -457,4 +457,102 @@ func reverse(head *ListNode) *ListNode{
 		head = tmp
 	}
 	return root
+}
+
+/***** 反转每对括号间的子串 *****/
+func reverseParentheses(s string) string {
+	// 字符串无法直接修改，转换为byte slice
+	brr := []byte(s)
+	var stack []int
+	for i := 0; i < len(brr); i ++ {
+		if brr[i] == '(' {
+			// 遇到左括号，加入栈中
+			stack = append(stack, i)
+		} else if brr[i] == ')'{
+			// 题目保证括号左右匹配，所以不用检验stack中是否有左括号
+			lastIdx := stack[len(stack)-1]
+			// 反转左括号位置+1到右括号位置-1之间的字符
+			for lj, rj := lastIdx + 1, i - 1; lj < rj; lj, rj = lj +1, rj -1 {
+				brr[lj], brr[rj] = brr[rj], brr[lj]
+			}
+			// 已匹配的左括号退栈
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	// 去掉所有括号字符
+	sb := strings.Builder{}
+	for i := 0; i < len(brr); i ++ {
+		if brr[i] != '(' && brr[i] !=')' {
+			sb.WriteByte(brr[i])
+		}
+	}
+
+	return sb.String()
+}
+
+/***** 接雨水 *****/
+func trap(height []int) int {
+	maxHeight := 0
+	for _, k := range height {
+		if k > maxHeight {
+			maxHeight = k
+		}
+	}
+
+	capacity := 0
+
+	for k := 1; k <= maxHeight; k++ {
+		pre := -1
+		for i := 0; i < len(height); i++ {
+			if height[i] < k {
+				continue
+			}
+			if pre == -1 {
+				pre = i
+			} else {
+				capacity += i - pre -1
+				pre = i
+			}
+		}
+	}
+	return capacity
+}
+
+/***** K 个一组翻转链表 *****/
+//给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+//k 是一个正整数，它的值小于或等于链表的长度。
+//如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	hair := &ListNode{Next: head}
+	pre := hair
+
+	for head != nil {
+		tail := pre
+		for i := 0; i < k; i++ {
+			tail = tail.Next
+			if tail == nil {
+				return hair.Next
+			}
+		}
+		nex := tail.Next
+		head, tail = myReverse(head, tail)
+		pre.Next = head
+		tail.Next = nex
+		pre = tail
+		head = tail.Next
+	}
+	return hair.Next
+}
+
+func myReverse(head, tail *ListNode) (*ListNode, *ListNode) {
+	prev := tail.Next
+	p := head
+	for prev != tail {
+		nex := p.Next
+		p.Next = prev
+		prev = p
+		p = nex
+	}
+	return tail, head
 }
