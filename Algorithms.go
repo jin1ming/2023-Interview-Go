0686ecb631524fa0a13a058ef3403b848d6bcca7
Dakini Wind <jin1ming@vip.qq.com>
2021-07-29 19:45:59 +0800

    7.29

--- Algorithms.go
+++ Algorithms.go
@@ -1651,3 +1651,74 @@ func flatten(root *TreeNode) {
 	lastNode.Left = nil
 	lastNode.Right = nil
 }
+
+/***** 汉明距离 *****/
+func hammingDistance(x int, y int) int {
+	xor := x ^ y
+	res := 0
+	for xor != 0 {
+		if xor % 2 == 1 {
+			res++
+		}
+		xor >>= 1
+	}
+	return res
+}
+
+/***** 找到字符串中所有字母异位词 *****/
+func findAnagrams(s string, p string) []int {
+	pl := len(p)
+	sl := len(s)
+	if pl > sl {
+		return nil
+	}
+	var result []int
+
+	m := make(map[byte]int)
+	for i := 0; i < pl; i++ {
+		m[p[i]]++
+	}
+
+	for i1 := 0; i1 < pl; i1++ {
+		m[s[i1]]--
+	}
+
+	out:
+	for i := 0; i < sl-pl+1; i++ {
+		if i > 0 {
+			m[s[i-1]]++
+			m[s[i+pl-1]]--
+		}
+
+		for _, v := range m {
+			if v != 0 {
+				continue out
+			}
+		}
+		result = append(result, i)
+	}
+	return result
+}
+
+/***** 会议室 II *****/
+func minMeetingRooms(intervals [][]int) int {
+	nums := make([]int, 0, 2 * len(intervals))
+	for _, v := range intervals {
+		nums = append(nums, v[0] * 10 + 2)
+		nums = append(nums, v[1] * 10 + 1)
+	}
+	sort.Ints(nums)
+	maxRoom := 0
+	curNeedRoom := 0
+	for _, v := range nums {
+		if v % 10 == 1 {
+			curNeedRoom--
+		} else {
+			curNeedRoom++
+		}
+		if curNeedRoom > maxRoom {
+			maxRoom = curNeedRoom
+		}
+	}
+	return maxRoom
+}
