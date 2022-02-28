b7393606d0b9eacc2441f211581f7f84648e6fbf
Dakini Wind <jin1ming@vip.qq.com>
2022-02-28 09:52:22 +0800

    2.28-2

--- algorithms/double_pointer.go
+++ algorithms/double_pointer.go
@@ -9,7 +9,7 @@ import (
 func twoSum(nums []int, target int) []int {
 	left, right := 0, len(nums)-1
 	indexs := make([]int, len(nums))
-	for i, _ := range indexs {
+	for i := range indexs {
 		indexs[i] = i
 	}
 	sort.Slice(indexs, func(i, j int) bool {
@@ -78,53 +78,37 @@ func threeSum(nums []int) [][]int {
 // 找出 nums 中的三个整数，使得它们的和与 target 最接近。
 // 返回这三个数的和。假定每组输入只存在唯一答案。
 func threeSumClosest(nums []int, target int) int {
-	sort.Ints(nums)
-	var (
-		n    = len(nums)
-		best = math.MaxInt32
-	)
+	if len(nums) < 3 {
+		return -1
+	}
 
-	// 根据差值的绝对值来更新答案
-	update := func(cur int) {
-		if abs(cur-target) < abs(best-target) {
-			best = cur
+	sort.Ints(nums) // 注意不要忘记排序
+	best := math.MaxInt32
+	checkAndUpdate := func(value int) {
+		if abs(value-target) < abs(best-target) {
+			// 注意不要忘记和上次比较时取差值的绝对值
+			best = value
 		}
 	}
 
-	// 枚举 a
-	for i := 0; i < n; i++ {
-		// 保证和上一次枚举的元素不相等
-		if i > 0 && nums[i] == nums[i-1] {
+	for k, v := range nums {
+		if k > 0 && v == nums[k-1] {
 			continue
 		}
-		// 使用双指针枚举 b 和 c
-		j, k := i+1, n-1
-		for j < k {
-			sum := nums[i] + nums[j] + nums[k]
-			// 如果和为 target 直接返回答案
+		left, right := k+1, len(nums)-1
+		for left < right {
+			sum := v + nums[left] + nums[right]
+			checkAndUpdate(sum)
 			if sum == target {
 				return target
-			}
-			update(sum)
-			if sum > target {
-				// 如果和大于 target，移动 c 对应的指针
-				k0 := k - 1
-				// 移动到下一个不相等的元素
-				for j < k0 && nums[k0] == nums[k] {
-					k0--
-				}
-				k = k0
+			} else if sum < target {
+				left++
 			} else {
-				// 如果和小于 target，移动 b 对应的指针
-				j0 := j + 1
-				// 移动到下一个不相等的元素
-				for j0 < k && nums[j0] == nums[j] {
-					j0++
-				}
-				j = j0
+				right--
 			}
 		}
 	}
+
 	return best
 }
 
