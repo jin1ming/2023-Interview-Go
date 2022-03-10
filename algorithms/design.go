8659eaba6af132ee4cbbf60c005da42b84ea5fd0
Dakini Wind <jin1ming@vip.qq.com>
2022-03-10 16:44:10 +0800

    2022.3.10

--- algorithms/design.go
+++ algorithms/design.go
@@ -5,6 +5,88 @@ import (
 	"time"
 )
 
+type LRUCache struct {
+	size       int
+	capacity   int
+	cache      map[int]*DLinkedNode
+	head, tail *DLinkedNode
+}
+
+type DLinkedNode struct {
+	key, value int
+	prev, next *DLinkedNode
+}
+
+func initDLinkedNode(key, value int) *DLinkedNode {
+	return &DLinkedNode{
+		key:   key,
+		value: value,
+	}
+}
+
+// Constructor2 /***** LRU 缓存 *****/
+func Constructor2(capacity int) LRUCache {
+	l := LRUCache{
+		cache:    map[int]*DLinkedNode{},
+		head:     initDLinkedNode(0, 0),
+		tail:     initDLinkedNode(0, 0),
+		capacity: capacity,
+	}
+	l.head.next = l.tail
+	l.tail.prev = l.head
+	return l
+}
+
+func (r *LRUCache) Get(key int) int {
+	if _, ok := r.cache[key]; !ok {
+		return -1
+	}
+	node := r.cache[key]
+	r.moveToHead(node)
+	return node.value
+}
+
+func (r *LRUCache) Put(key int, value int) {
+	if _, ok := r.cache[key]; !ok {
+		node := initDLinkedNode(key, value)
+		r.cache[key] = node
+		r.addToHead(node)
+		r.size++
+		if r.size > r.capacity {
+			removed := r.removeTail()
+			delete(r.cache, removed.key)
+			r.size--
+		}
+	} else {
+		node := r.cache[key]
+		node.value = value
+		r.moveToHead(node)
+	}
+}
+
+func (r *LRUCache) addToHead(node *DLinkedNode) {
+	node.prev = r.head
+	node.next = r.head.next
+	r.head.next.prev = node
+	r.head.next = node
+}
+
+func (r *LRUCache) removeNode(node *DLinkedNode) {
+	node.prev.next = node.next
+	node.next.prev = node.prev
+}
+
+func (r *LRUCache) moveToHead(node *DLinkedNode) {
+	r.removeNode(node)
+	r.addToHead(node)
+}
+
+func (r *LRUCache) removeTail() *DLinkedNode {
+	node := r.tail.prev
+	r.removeNode(node)
+	return node
+}
+
 type RandomizedSet struct {
 	nums []int       // 用于随机访问
 	M    map[int]int // 实现O(1)的访问和删除
