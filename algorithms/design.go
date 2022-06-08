2d665ba11ca0b4b661a9e3a89f637598d379daf7
Dakini Wind <jin1ming@vip.qq.com>
2022-06-08 17:09:37 +0800

    2022.6.8

--- algorithms/design.go
+++ algorithms/design.go
@@ -7,6 +7,40 @@ import (
 	"time"
 )
 
+const maxLen = 50002
+
+type StreamRank struct {
+	data [maxLen]int
+}
+
+// Constructor4 /***** 数字流的秩 *****/
+// 假设你正在读取一串整数。每隔一段时间，你希望能找出数字 x 的秩(小于或等于 x 的值的个数)。
+// 请实现数据结构和算法来支持这些操作，也就是说：
+// 实现 track(int x) 方法，每读入一个数字都会调用该方法；
+// 实现 getRankOfNumber(int x) 方法，返回小于或等于 x 的值的个数。
+// 思想：树状数组
+func Constructor4() StreamRank {
+	return StreamRank{}
+}
+
+func lowbit(x int) int {
+	return x & -x
+}
+
+func (this *StreamRank) Track(x int) {
+	for pos := x + 1; pos < maxLen; pos += lowbit(pos) {
+		this.data[pos]++
+	}
+}
+
+func (this *StreamRank) GetRankOfNumber(x int) int {
+	ans := 0
+	for pos := x + 1; pos > 0; pos -= lowbit(pos) {
+		ans += this.data[pos]
+	}
+	return ans
+}
+
 type LRUCache struct {
 	size       int
 	capacity   int
